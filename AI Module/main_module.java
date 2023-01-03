63f8ecb79b0cefeeedec509775c0de9b0cb3bb7b
Himanhshu-1433 <himanshu1433@tnraocollege.org>
2023-01-03 12:00:30 +0530

    save Ai

---
+++ AI Module/main_module.java
@@ -0,0 +1,17 @@
+import java.util.*;  
+class Greet{
+    public Greet(){
+        System.out.println("AI: Hello My Name Is Ruby, And I am AI Robot");
+        System.out.println("AI: How Can I Help You Please tell me some question:");
+        }
+}
+class Calling extends Greet{
+    public void Question(){
+        System.out.print("Me:");
+    }
+}
+public class main_module {
+    public static void main(String[] args) {
+        
+    }
+}
