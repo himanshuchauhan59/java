678edd311ce08779973d14884ada543234f4df74
Himanhshu-1433 <himanshu1433@tnraocollege.org>
2023-01-03 12:55:51 +0530

    save

--- AI Module/main_module.java
+++ AI Module/main_module.java
@@ -5,13 +5,36 @@ class Greet{
         System.out.println("AI: How Can I Help You Please tell me some question:");
         }
 }
+class AI{
+    public void Answer(String Text){
+        System.out.println(Text);
+    }
+}
 class Calling extends Greet{
+    AI Ans = new AI();    
     public void Question(){
-        System.out.print("Me:");
+        Scanner Question = new Scanner(System.in);
+        String Ques = " ";
+        while(Ques != "Exit"){
+            System.out.print("Me:");
+            Ques = Question.nextLine();        
+            if(Ques == " "){
+                Ans.Answer("Nothing I Am Exit");
+                Ques = "Exit";
+            }
+            else if(Ques!= " "){
+                String[] arr =  Ques.split(" ", 5);
+                // str.split("s", 0);
+                for(String a : arr){
+                    System.out.println("AI:"+a);
+                }
+            }
+        }
     }
 }
 public class main_module {
     public static void main(String[] args) {
-        
+        Calling obj = new Calling();
+        obj.Question();
     }
 }
