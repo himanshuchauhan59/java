1f66f34fcdb265b354c3e5dcf6c36102a70a7222
Himanhshu-1433 <himanshu1433@tnraocollege.org>
2023-01-03 13:45:08 +0530

    git pus

--- AI Module/main_module.java
+++ AI Module/main_module.java
@@ -5,28 +5,27 @@ class Greet{
         System.out.println("AI: How Can I Help You Please tell me some question:");
         }
 }
-class AI{
+class Calling extends Greet{
     public void Answer(String Text){
         System.out.println(Text);
     }
-}
-class Calling extends Greet{
-    AI Ans = new AI();    
     public void Question(){
         Scanner Question = new Scanner(System.in);
         String Ques = "";
-        while(Ques != "Exit"){
+        while(!Ques.equals("Exit")){
             System.out.print("Me:");
             Ques = Question.nextLine();        
-            if(Ques == ""){
-                Ans.Answer("Nothing I Am Exit");
+            if(Ques.equals("")){
+                Answer("Nothing I Am Exit");
                 Ques = "Exit";
             }
-            else if(Ques!= ""){
-                String[] arr =  Ques.split(" ", 5);
-                for(String a : arr){
-                    System.out.println("AI:"+a);
-                }
+            else if(!Ques.equals("")){
+                String[] arr =  Ques.split(" "); 
+                    for(String s : arr){
+                        if(s.equals("name")){
+                            System.out.println("hello "+arr[arr.length-1]);
+                        }
+                    }
             }
         }
     }
