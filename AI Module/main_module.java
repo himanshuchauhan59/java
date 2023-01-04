08703fd1767c003f334a2bdcab9150c775496f5f
Himanhshu-1433 <himanshu1433@tnraocollege.org>
2023-01-04 13:13:36 +0530

    Save Ai

--- AI Module/main_module.java
+++ AI Module/main_module.java
@@ -6,16 +6,41 @@ class Greet{
         }
 }
 class Calling extends Greet{
-    
-    
+    public void Answer(String Text){
+        System.out.println(Text);
+    }
     public void Question(){
         greetingUser greets = new greetingUser();
         Scanner Question = new Scanner(System.in);
         String Ques = "";
         while(!Ques.equals("Exit")){
             System.out.print("Me:");
-            Ques = Question.nextLine();        
-            greets.greet(Ques);
+            String txt = Question.nextLine();     
+            if(txt.equals("")){
+                Answer("Nothing I Am Exit");
+                txt = "Exit";
+            }
+            else if(!txt.equals("")){
+                String[] arr =  txt.split(" "); 
+                    for(String s : arr){
+                        if(s.equals("name") || s.equals("Name") || s.equals("NAME")){
+                            System.out.println("AI:hello "+arr[arr.length-1]);
+                        }
+                    } 
+                    String[] greetings_words = {"Hi" , "Hello" , "hi" , "hello" , "hey" , "Hey"};
+                    for(int start = 0; start <= greetings_words.length-1; start++)
+                    {
+                        if(arr[0].equals(greetings_words[start])){
+                        System.out.println("AI:"+arr[0]+" ");    
+                        break;
+                        }
+                        else if(!arr[0].equals(greetings_words[start]))
+                        {
+                                    
+                        }
+                    }
+                }   
+            
         }
     }
 }
